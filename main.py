f429d50a011a8b49d8ca8662bba6e5d13badd9a7
John <wooj7933@gmail.com>
2024-11-19 21:29:46 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+def draw_square(size):
+    for _ in rang
+
+
+screen.exitonclick()
